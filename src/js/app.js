169355544d93d1df1f1d951e3c56da7732d6bb9e
Raf <I447972@fhict.nl>
2021-12-21 22:10:31 +0100

    get user devices

--- src/js/app.js
+++ src/js/app.js
@@ -63,12 +63,18 @@ window.addEventListener("beforeinstallprompt", (event) => {
 
 //Spotify authentication
 var redirect_uri = "http://127.0.0.1:5500/src/index.html"
-var client_id = "";
-var client_secret = "";
+// TO-DO Hide client dat in env file
+var client_id = "f0f0c66ea501495e8e9755f63932633c";
+var client_secret = "afe7526d836b40079015b2c7c9673fe7";
 
 const AUTHORIZE = "https://accounts.spotify.com/authorize"
+const TOKEN = "https://accounts.spotify.com/api/token"
+const DEVICES = "https://api.spotify.com/v1/me/player/devices";
 
-function onPagLoad() {
+function onPageLoad() {
+    // client_id = localStorage.getItem("client_id");
+    // client_secret = localStorage.getItem("client_secret");
+
     if (window.location.search.length > 0) {
         handleRedirect();
     }
@@ -77,6 +83,8 @@ function onPagLoad() {
 function handleRedirect() {
     let code = getCode();
     fetchAccessToken(code);
+    //Remove parameters from URL
+    window.history.pushState("", "", redirect_uri)
 }
 
 function fetchAccessToken(code) {
@@ -91,7 +99,33 @@ function fetchAccessToken(code) {
 function callAuthorizationApi(body) {
     let xhr = new XMLHttpRequest();
     xhr.open("POST", TOKEN, true);
-    // !!!!!!!!!!!!!!!!!!!!!
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret));
+    xhr.send(body);
+    xhr.onload = handleAuthorizationResponse;
+}
+
+function handleAuthorizationResponse() {
+    //Check for a succesfull response
+    if (this.status == 200) {
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        var data = JSON.parse(this.responseText);
+        //Make sure correct tokens were recieved
+        if (data.access_token != undefined) {
+            access_token = data.access_token
+            localStorage.setItem("access_token", access_token);
+        }
+        if (data.refresh_token != undefined) {
+            refresh_token = data.refresh_token;
+            localStorage.setItem("refresh_token", refresh_token);
+        }
+        onPageLoad();
+    }
+    else {
+        console.log(this.responseText);
+        alert(this.responseText)
+    }
 }
 
 function getCode() {
@@ -107,9 +141,8 @@ function getCode() {
 }
 
 function requestAuthorization() {
-    client_id = document.getElementById("clientId").value;
-    client_secret = document.getElementById("clientSecret").value;
-    // TO-DO hide client data in env file
+    // client_id = document.getElementById("clientId").value;
+    // client_secret = document.getElementById("clientSecret").value;
     localStorage.setItem("client_id", client_id);
     localStorage.setItem("client_secret", client_secret);
 
@@ -121,4 +154,55 @@ function requestAuthorization() {
     url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
     // Show Spotify authorization window
     window.location.href = url;
+}
+
+function refreshDevices() {
+    callApi("GET", DEVICES, null, handleDevicesResponse);
+}
+
+function handleDevicesResponse() {
+    if (this.status == 200) {
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        removeAllItems("devices");
+        data.devices.forEach(item => addDevice(item))
+    }
+    else if (this.status == 401) {
+        refresAccesstoken()
+    }
+    else {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }
+}
+
+function refreshAccessToken() {
+    refresh_token = localStorage.getItem("refresh_token");
+    let body = "grant_type=refresh_token";
+    body += "&refresh_token=" + refresh_token;
+    body += "&client_id=" + client_id;
+    callAuthorizationApi(body);
+}
+
+function addDevice(item) {
+    let node = document.createElement("option");
+    node.value = item.id;
+    node.innerHTML = item.name;
+    document.getElementById("devices").appendChild(node);
+}
+
+function callApi(method, url, body, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+    xhr.send(body);
+    xhr.onload = callback;
+}
+
+function removeAllItems(elementId) {
+    let node = document.getElementById(elementId);
+    while (node.firstChild) {
+        node.removeChild(node.firstChild);
+    }
 }
